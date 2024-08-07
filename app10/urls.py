739dbe609dde33c6e8beec38eddc53a59fd1ffed
vaisakhcsunil <vaisakhcsunil@gmail.com>
2024-08-07 16:38:21 +0530

    first commit

---
+++ app10/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('shop/',views.shop,name='shop'),
+    path('detail/',views.detail,name='detail'),
+    path('cart/',views.cart,name='cart'),
+    path('checkout/',views.checkout,name='checkout'),
+    path('contact/',views.contact,name='contact')
+]
---
+++ app10/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+def shop(request):
+    return render(request,'shop.html')
+def detail(request):
+    return render(request,'detail.html')
+def cart(request):
+    return render(request,'cart.html')
+def checkout(request):
+    return render(request,'checkout.html')
+def contact(request):
+    return render(request,'contact.html')
